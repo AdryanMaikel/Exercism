c9c6fca0d2ed92f40745781975e535e4c0e036a3
NGS-Adryan <ngs.adryan@gmail.com>
2024-02-08 17:13:49 -0300

    Completando bob
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ python/bob/bob.py
@@ -0,0 +1,11 @@
+def response(hey_bob: str) -> str:
+    hey_bob = hey_bob.strip()
+    if hey_bob.isupper():
+        if hey_bob.endswith("?"):
+            return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+    if hey_bob.endswith("?"):
+        return "Sure."
+    if not hey_bob:
+        return "Fine. Be that way!"
+    return "Whatever."
