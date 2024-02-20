2f418f90e451cd578447509f0d914ff797d7c0b1
NGS-Adryan <ngs.adryan@gmail.com>
2024-02-20 14:29:35 -0300

    completando reverse string
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ python/reverse-string/reverse_string.py
@@ -0,0 +1,6 @@
+def reverse(text: str) -> str:
+    """Function that reverses the text.
+    :param text: str - Text to be reversed.
+    :return: str - Inverted text.
+    """
+    return "".join(reversed(text))
