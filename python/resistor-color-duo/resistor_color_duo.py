a3d81946204f0add1124823b503178e34dbbd0bd
NGS-Adryan <ngs.adryan@gmail.com>
2024-02-22 16:16:03 -0300

    melhorando nome da variavel
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,6 +1,6 @@
-COLORS = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
-          "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+COLOR_BANDS = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
+               "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
 
 
 def value(colors: list[str]) -> int:
-    return int("".join([str(COLORS[color]) for color in colors[:2]]))
+    return int("".join([str(COLOR_BANDS[color]) for color in colors[:2]]))
--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,10 +1,10 @@
-COLORS = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
-          "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+COLOR_BANDS = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
+               "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
 
 
 def color_code(color: str) -> int:
-    return COLORS[color]
+    return COLOR_BANDS[color]
 
 
 def colors():
-    return list(COLORS.keys())
+    return list(COLOR_BANDS.keys())
