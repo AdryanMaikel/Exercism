968cec827e30a617867ce7b1c946fbeb2c4bb220
NGS-Adryan <ngs.adryan@gmail.com>
2024-02-26 16:03:33 -0300

    completando square_root
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ python/square-root/square_root.py
@@ -0,0 +1,10 @@
+from math import isqrt
+
+
+def square_root(number: int) -> int:
+    """Get the square root of the number.
+
+    :param number: int - Number to get the square root.
+    :return: int - Square root in integer.
+    """
+    return isqrt(number)
