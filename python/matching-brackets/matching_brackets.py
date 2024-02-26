55ee421b83ba717d26142097050ca8a1e2fbc23d
NGS-Adryan <ngs.adryan@gmail.com>
2024-02-26 16:36:19 -0300

    add docstring
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,4 +1,9 @@
-def is_paired(input_string):
+def is_paired(input_string: str) -> bool:
+    """Check if this string contains valid {}[]() pair.
+
+    :param input_string: str - String to be checked.
+    :return: bool - Whether it contains valid pairs.
+    """
     chars = "".join([char for char in input_string if char in "{}[]()"])
     for _ in range(2):
         for replace in ["{}", "[]", "()"]:
